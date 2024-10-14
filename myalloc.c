678d429561a3ad727fa2a2e6e3dd635f14539fff
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2024-10-14 10:01:51 +0200

    clean code

--- myalloc.c
+++ myalloc.c
@@ -1,21 +1,5 @@
 #include "myalloc.h"
 
-// void* my_alloc(size_t size)
-// {
-//     if(size <= SIZE_BLK_SMALL)
-//     {
-//         for(int i = 0; i < MAX_SMALL; i++)
-//         {
-//             if(IS_FREE(small_tab[i].body))
-//             {
-//                 small_tab[i].head += 1;
-//                 return small_tab[i].body;
-//             }
-//         }
-//     }
-//     return NULL;
-// }
-
 void* my_alloc(size_t size)
 {
     if(size <= SIZE_BLK_SMALL && list_block != NULL)
@@ -27,26 +11,6 @@ void* my_alloc(size_t size)
     }
     return NULL;
 }
-
-// void my_free(void* ptr)
-// {
-//     size_t new_small_tab = (size_t)small_tab;
-//     size_t new_ptr = (size_t)ptr;
-
-//     if(new_small_tab > new_ptr || (new_ptr > (new_small_tab + sizeof(block_t) * MAX_SMALL)))
-//     {
-//         printf("The pointer is outside the working memory area.\n");
-//     }else if((new_ptr - new_small_tab) % sizeof(block_t) != sizeof(size_t))
-//     {
-//         printf("The pointer isn't at the begining of a block.\n");
-//     }else if(IS_FREE(ptr))
-//     {
-//         printf("The pointer is in an empty block.\n");
-//     }else
-//     {
-//         HEAD(ptr) -= 1;
-//     }
-// }
 
 void my_free(void* ptr)
 {
@@ -68,24 +32,6 @@ void my_free(void* ptr)
         list_block = ptr_head(ptr);
     }
 }
-
-// void* my_realloc(void* ptr, size_t size)
-// {
-//     size_t new_small_tab = (size_t)small_tab;
-//     size_t new_ptr = (size_t)ptr;
-
-//     if(new_small_tab > new_ptr || (new_ptr > (new_small_tab + sizeof(block_t) * MAX_SMALL)))
-//     {
-//         return NULL;
-//     }else if((new_ptr - new_small_tab) % sizeof(block_t) != sizeof(size_t))
-//     {
-//         return NULL;
-//     }else if(size > SIZE_BLK_SMALL || IS_FREE(ptr))
-//     {
-//         return NULL;
-//     }
-//     return ptr;
-// }
 
 void* my_realloc(void* ptr, size_t size)
 {
